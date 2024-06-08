80c7f7b1dcfd900b015fea035df586d93a3c7fa0
Nishant Tandon <113603880+9HsTs@users.noreply.github.com>
2024-06-08 19:16:26 +0530

    CODSOFT

---
+++ Login page/my_app/lib/Utilis/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+Color backgroundColor1 = const Color(0xffE9EAF7);
+Color primaryColor = const Color.fromARGB(255, 90, 77, 204);
+Color backgroundColor2 = const Color(0xffF4EEF2);
+Color backgroundColor3 = const Color(0xffEBEBF2);
+Color backgroundColor4 = const Color(0xffE3EDF5);
+Color textColor1 = const Color(0xff353047);
+Color textColor2 = const Color(0xff6F6B7A);
+Color buttonColor = const Color.fromARGB(255, 102, 80, 213);
